16fa8ddd50ceccccf738ad594c520072699a5644
Brian King <kibri@ohsu.edu>
2016-12-19 17:16:23 -0800

    add protobuf dependency. only delete directories for generated json schemas

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(name='Bioschemas',
       packages=find_packages(),
       install_requires=[
           'jsonschema',
+          'protobuf'
       ],
       test_suite='nose.collector',
       tests_require=['nose'],
